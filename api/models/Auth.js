8923956c5b506a6589ec6ada79d59e4d7b606622
kavindu heshan <rakhr23110@gmail.com>
2018-05-19 12:40:14 +0530

    init commit

---
+++ api/models/Auth.js
@@ -0,0 +1,36 @@
+/**
+ * Auth.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+const bcrypt = require("bcrypt-nodejs");
+module.exports = {
+  attributes: {
+
+
+    password: {
+      type: "string",
+      isNotEmptyString: true
+    },
+    username: {
+      type: "string",
+      //unique: true
+    }
+  },
+  tableName: "user",
+
+  customToJSON: function() {
+    return _.omit(this, ["password"]);
+  },
+  beforeCreate: function(user, cb) {
+    bcrypt.genSalt(10, function(err, salt) {
+      bcrypt.hash(user.password, salt, null, function(err, hash) {
+        if (err) return cb(err);
+        user.password = hash;
+        return cb();
+      });
+    });
+  }
+};
+
